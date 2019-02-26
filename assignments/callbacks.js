116289dd8d226d74e9613afca2698908fe7252d2
dgbailey <bailey.dust@gmail.com>
2019-02-26 12:53:51 -0800

    completed callbacks no stretch for now

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -10,6 +10,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     // firstItem passes the first item of the given array to the callback function.
   }
 
+
   // Potential Solution:
 
   // Higher order function using "cb" as the call back
@@ -27,25 +28,43 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, len => console.log(len));
+
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  console.log(last.name);
+  return cb((arr[arr.length -1]));
 }
+last(items, li => console.log(li));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  console.log(sumNums.name);
+  return cb(x+y);
 }
+sumNums(2,3, input => console.log(input));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  console.log(multiplyNums.name);
+  return cb(x * y)
+
 }
 
+multiplyNums(2,2, product => console.log(product));
+
+
 function contains(item, list, cb) {
+  return cb(list.includes(item));
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
-
+console.log(contains.name);
+contains('Pencil',items, x => console.log(x));
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
